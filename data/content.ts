8602ad07af54755e3410f1104549404134e33518
fathur-rs <fathurrahmansyarief@gmail.com>
2023-06-10 14:17:51 +0700

    delete data skills

--- data/content.ts
+++ data/content.ts
@@ -59,39 +59,6 @@ export const contentData = [
     },
 ];
 
-export const  contentDataSkill = [
-  {
-    title: "Projects",
-    items: [
-      {
-        title: "Banking Marketing Targets",
-        subTitle: "Data Analytics",
-        date: "2020",
-        description: "End-to-End Data Analysis. Pulling data with pySpark. Exploratory Data Analysis (EDA), Data Visualization, implementing various machine learning algorithms achieving 90% average accuracy, and model evaluation.",
-      },
-      {
-        title: "Tokopedia Flash Sale Catalogue Scrapper",
-        subTitle: "Data Mining",
-        date: "2021",
-        description: "Fast runtime E-Commerce Flash Sale Catalogue Scrapper using Selenium & BeautifulSoup framework.",
-      },
-      {
-        title: "Tweetoxicity — ML  Sentiment Based",
-        subTitle: "Web App",
-        date: "2022",
-        description: "ML-powered behavioral analysis web application. Real-time calculating Twitter account sentiment analysis using ensemble machine learning algorithm achieving 95% accuracy.",
-      },
-      {
-        title: "Trxnslate — Automated Handwritten Prescription Recognition",
-        subTitle: "Web App",
-        date: "2023",
-        description: "Utilizes CNN VGG 16 Transfer Learning Model achieving 98% accuracy, The web app allows users to upload prescription images, generating digital text output within seconds. The project incorporates Kubeflow for MLops pipeline and deployed on GCP.",
-      },
-    ],
-  },
-];
-
-
 export type Content = {
   title: string;
   items: {
