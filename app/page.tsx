e979bcde557e97cfe93a4d30869b4a797850c409
fathur-rs <fathurrahmansyarief@gmail.com>
2023-06-10 15:17:26 +0700

    rework project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import ThemeSwitch from "@/components/theme-switch";
 import { generalData } from "@/data/general";
 import { contentData } from "@/data/content";
 import type { Content } from "@/data/content";
+import { projectData } from "@/data/project";
+import type { ContentProject } from "@/data/project";
 
 type ContentProps = Content;
 
@@ -19,6 +20,56 @@ const Content: React.FC<ContentProps> = ({ title, items }) => {
               </div>
               <div className="flex flex-col flex-1">
                 <h4>{item.title}</h4>
+                
+                <p className="text-slate-600 dark:text-gray-400">
+                  {item.subTitle}
+                </p>
+                {item.description ? (
+                  <p className="text-slate-600 dark:text-gray-400 mt-2">
+                    {item.description}
+                  </p>
+                ) : null}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+const ContentProject: React.FC<ContentProps> = ({ title, items }) => {
+  return (
+    <section className="my-14 text-sm">
+      <h3 className="mb-6">{title}</h3>
+      <div className="flex flex-col gap-6">
+        {items.map((item, index) => {
+          return (
+            <div className="flex" key={index}>
+              <div className="mr-8 max-w-[100px] lg:max-w-[120px] w-full text-slate-400 dark:text-slate-400">
+                {item.date}
+              </div>
+              <div className="flex flex-col flex-1">
+                <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:underline inline-flex"
+                    >
+                      {item.title}
+                      <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
+                          className="fill-current text-slate-900 dark:text-slate-100"
+                        ></path>
+                      </svg>
+                </a>
                 <p className="text-slate-600 dark:text-gray-400">
                   {item.subTitle}
                 </p>
@@ -43,7 +94,7 @@ export default function Home() {
         <section className="flex items-center">
           <Image
             alt="Author"
-            src='/icon-Dau5BeEtQq-transformed.jpeg'
+            src="/icon-Dau5BeEtQq-transformed.jpeg"
             width={80}
             height={80}
             className="rounded-full object-cover"
@@ -64,7 +115,9 @@ export default function Home() {
           </div>
         </section>
         <section className="my-14 text-sm">
-          <h3 className="mb-1 text-slate-900 dark:text-slate-100 mb-6">Skills</h3>
+          <h3 className="mb-1 text-slate-900 dark:text-slate-100 mb-6">
+            Skills
+          </h3>
           <div className="wrapper">
             <div className="flex gap-4 flex-wrap">
               <p className="border border-slate-600 px-6 py-2 text-slate-300 hover:text-slate-100 hover:border-slate-300 rounded-lg duration-300">
@@ -118,6 +171,9 @@ export default function Home() {
         {contentData.map((content, index) => {
           return <Content {...content} key={index} />;
         })}
+        {projectData.map((content, index) => {
+          return <ContentProject {...content} key={index} />;
+        })}
         <section className="my-14 text-sm">
           <h3 className="mb-6 text-slate-900">Contact</h3>
           <div className="flex flex-col gap-6">
@@ -154,7 +210,6 @@ export default function Home() {
             })}
           </div>
         </section>
-
       </main>
     </>
   );
---
+++ data/project.ts
@@ -0,0 +1,48 @@
+export const projectData = [
+      {
+        title: "Projects",
+        items: [
+          {
+            title: "Banking Marketing Targets",
+            subTitle: "Data Analytics",
+            date: "2020",
+            description: "End-to-End Data Analysis. Pulling data with pySpark. Exploratory Data Analysis (EDA), Data Visualization, implementing various machine learning algorithms achieving 90% average accuracy, and model evaluation.",
+            url: "https://www.kaggle.com/code/fathurrs/banking-dataset-marketing-targets",
+          },
+          {
+            title: "Tokopedia Flash Sale Catalogue Scrapper",
+            subTitle: "Data Mining",
+            date: "2021",
+            description: "Fast runtime E-Commerce Flash Sale Catalogue Scrapper using Selenium & BeautifulSoup framework.",
+            url: "https://github.com/fathur-rs/tokopedia_flash_sales_scrapper/blob/master/TokopediaFlashSalesScrapper.py",
+          },
+          {
+            title: "Tweetoxicity — Sentiment Analysis",
+            subTitle: "Web App",
+            date: "2022",
+            description: "ML-powered behavioral analysis web application. Real-time calculating Twitter account sentiment analysis using ensemble machine learning algorithm achieving 95% accuracy.",
+            url: "https://github.com/fathur-rs/Tweetoxicity",
+          },
+          {
+            title: "Trxnslate — Handwritten Prescription Recognition",
+            subTitle: "Web App",
+            date: "2023",
+            description: "Utilizes CNN VGG 16 Transfer Learning Model achieving 98% accuracy, The web app allows users to upload prescription images, generating digital text output within seconds. The project incorporates Kubeflow for MLops pipeline and deployed on GCP.",
+            url: "http://bit.ly/TRxNLATE",
+          },
+        ],
+      },
+  ];
+
+  export type Content = {
+    title: string;
+    items: {
+      title: string;
+      subTitle: string;
+      date: string;
+      description: string;
+      url: string;
+    }[];
+  };
+
+  export type ContentProject = Content[];
